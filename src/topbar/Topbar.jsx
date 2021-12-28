266558b365e33bd3227541f143ee1e487e782e88
adriano-r <adrianorosa9974@gmail.com>
2021-12-28 17:41:53 -0300

    navbar

---
+++ src/topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import "./topbar.css";
+
+export default function Topbar() {
+	return (
+		<div className="top">
+			<div className="topLeft">
+				<i class="fab fa-facebook-square"></i>
+			</div>
+			<div className="topCenter">center</div>
+			<div className="topRight">right</div>
+		</div>
+	);
+}
