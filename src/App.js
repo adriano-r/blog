95900b204fe99c43dfa5bbb16d99b62b328cac90
adriano-r <adrianorosa9974@gmail.com>
2021-12-28 23:16:33 -0300

    login and register pages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
 	return (
 		<>
 			<Topbar />
-			<Settings />
+			<Register />
 		</>
 	);
 }
