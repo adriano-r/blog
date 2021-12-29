5ebed5a38e99e6b7b249666a2bd34c09b4774371
adriano-r <adrianorosa9974@gmail.com>
2021-12-29 00:14:52 -0300

    front ok

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+	const user = false;
 	return (
-		<>
+		<BrowserRouter>
 			<Topbar />
-			<Register />
-		</>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				<Route path="/register" element={user ? <Home /> : <Register />} />
+				<Route path="/login" element={user ? <Home /> : <Login />} />
+				<Route path="/write" element={user ? <Write /> : <Register />} />
+				<Route path="/settings" element={user ? <Settings /> : <Register />} />
+				<Route path="/post/:postId" element={<Single />} />
+			</Routes>
+		</BrowserRouter>
 	);
 }
 
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./topbar.css";
 
 export default function Topbar() {
+	const user = false;
 	return (
 		<div className="top">
 			<div className="topLeft">
@@ -11,19 +13,50 @@ export default function Topbar() {
 			</div>
 			<div className="topCenter">
 				<ul className="topList">
-					<li className="topListItem">HOME</li>
-					<li className="topListItem">ABOUT</li>
-					<li className="topListItem">CONTACT</li>
-					<li className="topListItem">WRITE</li>
-					<li className="topListItem">LOGOUT</li>
+					<li className="topListItem">
+						<Link className="link" to="/">
+							HOME
+						</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/">
+							ABOUT
+						</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/">
+							CONTACT
+						</Link>
+					</li>
+					<li className="topListItem">
+						<Link className="link" to="/write">
+							WRITE
+						</Link>
+					</li>
+					<li className="topListItem">{user && "LOGOUT"}</li>
 				</ul>
 			</div>
 			<div className="topRight">
-				<img
-					className="topImg"
-					src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fbe%2Fc6%2F38%2Fbec638042941ca7997d296b041a13de2.jpg&f=1&nofb=1"
-					alt=""
-				/>
+				{user ? (
+					<img
+						className="topImg"
+						src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fbe%2Fc6%2F38%2Fbec638042941ca7997d296b041a13de2.jpg&f=1&nofb=1"
+						alt=""
+					/>
+				) : (
+					<ul className="topList">
+						<li className="topListItem">
+							<Link className="link" to="/login">
+								LOGIN
+							</Link>
+						</li>
+						<li className="topListItem">
+							<Link className="link" to="/register">
+								REGISTER
+							</Link>
+						</li>
+					</ul>
+				)}
 				<i className="topSearchIcon fas fa-search"></i>
 			</div>
 		</div>
